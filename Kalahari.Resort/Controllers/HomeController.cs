9c8868e85af86abc3edd7f2cb2a9ca84f92e179d
Ayende Rahien <ayende@ayende.com>
2012-01-09 09:27:07 -0500

    Adding a room entity and saving it to the database. Session/store management is intentionally bad - do not use this method

---
+++ Kalahari.Resort/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using Kalahari.Resort.Models;
+using Raven.Client.Document;
+
+namespace Kalahari.Resort.Controllers
+{
+	public class HomeController : Controller
+	{
+		public  ActionResult Index()
+		{
+			using(var store = new DocumentStore
+			{
+				Url = "http://localhost:8080"
+			}.Initialize())
+			{
+				using(var session = store.OpenSession())
+				{
+					session.Store(new Room
+					{
+						Number = "2556",
+						Rating = 4,
+						RoomType = "Double Bed",
+						Smoking = false
+					});
+
+					session.SaveChanges();
+				}
+			}
+			return Json(new {ok = true}, JsonRequestBehavior.AllowGet);
+		}
+	}
+}
---
+++ Kalahari.Resort/Models/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace Kalahari.Resort.Models
+{
+	public class Room
+	{
+		public string Id { get; set; }
+		public string Number { get; set; }
+		public bool Smoking { get; set; }
+		public int Rating { get; set; }
+		public string RoomType { get; set; }
+	}
+}
